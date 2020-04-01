ef87cc4f7cc667b20f6edc70f0e4a8dd2b241f7d
JohnEnevoldsen <62882814+JohnEnevoldsen@users.noreply.github.com>
2020-04-01 11:03:53 +0200

    Tilføjet to objeckter

--- Program.cs
+++ Program.cs
@@ -9,8 +9,8 @@ namespace GitExercise
             Console.WriteLine("Hello World!");
 
             myClass myclass = new myClass("John", "Nørregade");
-            
-
+            myClass nytObjekt = new myClass("Gleb" , "Et sted i Danmark");
+            myClass etTilObjekt = new myClass("Simon", "København");
 
         }
     }
