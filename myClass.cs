17452dee7b63ece799e21ec1ed78307e900cdb11
JohnEnevoldsen <62882814+JohnEnevoldsen@users.noreply.github.com>
2020-04-01 10:46:12 +0200

    Tilføjet properties
    Tilføjet en Navn property
    Tilføjet en Adresse property
    Tilføjet en PostNr property

--- myClass.cs
+++ myClass.cs
@@ -2,6 +2,20 @@ namespace GitExercise
 {
     class myClass
     {
-        
+        private string _navn;
+        private string _adresse;
+        private int _postNr;
+        public string Navn{
+            get{return _navn;}
+            set{_navn = value;}
+        }
+        public string Adresse{
+            get{return _adresse;}
+            set{_adresse = value;}
+        }
+        public int PostNr{
+            get{return _postNr;}
+            set{_postNr = value;}
+        }
     }
 }
